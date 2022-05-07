648d37af36f28bf5b584a8bfed898d4c1a6cc8d2
brokenken <acetek.50@gmail.com>
2022-05-07 22:19:17 +0300

    Vmute + Cmute

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 settings = {
     'prefix': '!',
-    'guild_ids': [971085376758370405]
+    'guild_ids': [961360073983144016, 971085376758370405],
+    'chat_mute_role_id': 971487061624377366,
+    'voice_mute_role_id': 972266735833063455,
+    'channel_logs_id': 972249452205199400,
+    'moderation_role': [972572343673163827]
 }
