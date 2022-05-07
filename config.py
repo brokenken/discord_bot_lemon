3cc88e8208654d84f5ca61a68fe516849e4721b0
frungl <fruuungl@gmail.com>
2022-05-08 00:35:46 +0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
     'prefix': '!',
-    'guild_ids': [961360073983144016, 971085376758370405],
+    'guild_ids': [961360073983144016],
     'chat_mute_role_id': 971487061624377366,
     'voice_mute_role_id': 972266735833063455,
     'channel_logs_id': 972249452205199400,
