809468e1461ec6bf99f81479d87c1798537c6482
brokenken <acetek.50@gmail.com>
2022-05-20 00:22:42 +0300

    Казино, профиль, аватар

--- commands/Entertainment.py
+++ commands/Entertainment.py
@@ -3,14 +3,13 @@ from discord.ext import commands
 from discord_slash import cog_ext
 from discord_slash.utils.manage_commands import create_option
 from config import settings
-from discord.ext import tasks
 import json
+from random import randint
 
 
 class Entertainment(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.check_users.start()
 
     @cog_ext.cog_slash(
         name='avatar',
@@ -27,25 +26,70 @@ class Entertainment(commands.Cog):
         embed.set_image(url=member.avatar_url)
         await ctx.send(embed=embed)
 
-    @tasks.loop(seconds=5)
-    async def check_users(self):
-        for guild in self.bot.guilds:
-            for member in guild.members:
-                with open('.\\databases\\user_data.json', 'r') as file:
-                    user_data = json.load(file)
-                    new_user = str(member.id)
-                if new_user not in user_data:
-                    user_data[new_user] = 0
-                    with open('.\\databases\\user_data.json', 'w') as new_user_data:
-                        json.dump(user_data, new_user_data, indent=4)
-                if member.voice is not None:
-                    user_data[new_user] += 5
-                    with open('.\\databases\\user_data.json', 'w') as update_user_data:
-                        json.dump(user_data, update_user_data, indent=4)
+    @cog_ext.cog_slash(
+        name='profile',
+        description='shows user profile',
+        options=[
+            create_option(name="member", description="select user", required=False, option_type=6)
+        ],
+        guild_ids=settings['guild_ids']
+    )
+    @commands.command()
+    async def avatar(self, ctx, member=None):
+        if member is None:
+            member = ctx.author
+        with open('.\\databases\\user_data.json', 'r') as file:
+            user_data = json.load(file)
+            new_user = str(member.id)
+        cnt = 0
+        for user in user_data:
+            if user == new_user:
+                cnt = user_data[new_user][0]
+        embed = discord.Embed(tittle='Профиль', description='Профиль <@{}>'.format(member.id), color=0xec186d)
+        h = (cnt // 60) // 60
+        m = cnt // 60
+        s = cnt - (h * 60 * 60 + m * 60)
+        if h == 0:
+            if m == 0:
+                embed.add_field(name='Время в войсе', value='`{} секунд`'.format(s), inline=False)
+            else:
+                embed.add_field(name='Время в войсе', value='`{} минут {} секунд`'.format(m, s), inline=False)
+        else:
+            embed.add_field(name='Время в войсе', value='`{} часов {} минут {} секунд'.format(h, m, s), inline=False)
+        embed.add_field(name='Количество сообщений', value='`{} сообщений`'.format(user_data[new_user][1]), inline=False)
+        embed.add_field(name='Баланс', value='`{}` :coin:'.format(user_data[new_user][2]), inline=False)
+        embed.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=embed)
 
-    @check_users.before_loop
-    async def before_my_task(self):
-        await self.bot.wait_until_ready()
+    @cog_ext.cog_slash(
+        name='slot',
+        description='casino',
+        options=[
+            create_option(name="count", description="pot", required=True, option_type=4)
+        ],
+        guild_ids=settings['guild_ids']
+    )
+    @commands.command()
+    async def slot(self, ctx, count):
+        member = ctx.author
+        with open('.\\databases\\user_data.json', 'r') as file:
+            user_data = json.load(file)
+            new_user = str(member.id)
+        if count > user_data[new_user][2]:
+            await ctx.send("Мало на счёте")
+        else:
+            user_data[new_user][2] -= count
+            x = randint(1, 100)
+            if x >= 90:
+                user_data[new_user][2] += count * 2
+                await ctx.send("Победа!")
+            else:
+                await ctx.send("Казино....")
+            with open('.\\databases\\user_data.json', 'w') as update_user_data:
+                json.dump(user_data, update_user_data, indent=4)
+
+
+
 
 
 def setup(bot):
